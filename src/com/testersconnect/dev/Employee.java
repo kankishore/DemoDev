3344095a287a47dd08fb845683a8b555ffd1f8e0
kkuruvadi <kishore.kuruvadi@aptean.com>
2018-03-20 16:48:47 +0530

    Initial Commit....

---
+++ src/com/testersconnect/dev/Employee.java
@@ -0,0 +1,15 @@
+package com.testersconnect.dev;
+
+public class Employee {
+
+	
+	
+	public String getEmployeeName(int eNo) {
+		String eName=null;
+		eName="John";
+		
+		return eName;
+	}
+	
+	
+}
